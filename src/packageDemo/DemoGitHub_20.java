921f8700db13757438a5a7608b4331157521c842
PCV <PCV@DESKTOP-3R7I1A9>
2017-03-18 00:29:31 +0100

    Creación del Bucle for.

--- src/packageDemo/DemoGitHub_20.java
+++ src/packageDemo/DemoGitHub_20.java
@@ -2,5 +2,8 @@ package packageDemo;
 public class DemoGitHub_20 {
     public static void main(String[] args) {
         System.out.println("Este programa contará hasta Diez");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(""+i);
+        }
     }   
 }
